cc48b3281edb8547cbe6e5399982f7eb0f698218
Taryn Hill <taryn@phrohdoh.com>
2017-09-30 10:34:01 -0500

    Force tests to fail because no meaningful tests have been written

--- src/lib.rs
+++ src/lib.rs
@@ -68,7 +68,8 @@ impl Pokemon {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn todo() {
+        // TODO: Write tests!
+        assert!(false);
     }
 }
